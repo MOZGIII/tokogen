42ec5411e1bd6c3a1055ee9679011a7964a4e7a8
MOZGIII <mike-n@narod.ru>
2017-01-11 16:17:38 +0300

    Added more tests

--- spec/tokogen_spec.rb
+++ spec/tokogen_spec.rb
@@ -5,7 +5,9 @@ describe Tokogen do
   it 'has a version number' do
     expect(Tokogen::VERSION).not_to be nil
   end
+end
 
+describe Tokogen, '.score' do
   it 'generates token of the correct length' do
     (0..1024).each do |length|
       expect(Tokogen.token(length).size).to eq length
@@ -14,5 +16,39 @@ describe Tokogen do
 
   it 'generates 32 length token without arguments' do
     expect(Tokogen.token.size).to eq 32
+  end
+end
+
+describe Tokogen, '.default_generator' do
+  it 'build a generator' do
+    expect(Tokogen.default_generator).to be_kind_of Tokogen::Generator
+  end
+
+  it 'build a generator with SecureRandom randomness source' do
+    generator = Tokogen.default_generator
+    expect(generator.randomness_source).to eq SecureRandom
+  end
+end
+
+
+describe Tokogen, '.generator' do
+  it 'build a generator' do
+    expect(Tokogen.generator).to be_kind_of Tokogen::Generator
+  end
+
+  it 'can be called without options' do
+    expect { Tokogen.generator }.to_not raise_error
+  end
+
+  it 'can be called with options' do
+    expect { Tokogen.generator(randomness_source: :test_randomness_source) }.to_not raise_error
+    expect { Tokogen.generator(alphabet: 'ABC') }.to_not raise_error
+    expect { Tokogen.generator(randomness_source: :test_randomness_source, alphabet: 'ABC') }.to_not raise_error
+  end
+
+  it 'build different objects (does not cache anything)' do
+    one = Tokogen.generator
+    two = Tokogen.generator
+    expect(one).to_not eq two
   end
 end
