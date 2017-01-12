0df9a27d0d9339f39901251d214d3d5c7b5d6ca6
MOZGIII <mike-n@narod.ru>
2017-01-12 15:43:40 +0300

    Fixed rubocop offense at Tokogen::Alphabet::BASE58 definition

--- lib/tokogen/alphabet.rb
+++ lib/tokogen/alphabet.rb
@@ -2,7 +2,7 @@
 module Tokogen
   module Alphabet
     BASE62 = (('A'..'Z').to_a + ('a'..'z').to_a + ('0'..'9').to_a).join.freeze
-    BASE58 = (('A'..'Z').to_a + ('a'..'z').to_a + ('0'..'9').to_a - ['0', 'O', 'I', 'l']).join.freeze
+    BASE58 = (('A'..'Z').to_a + ('a'..'z').to_a + ('0'..'9').to_a - %w(0 O I l)).join.freeze
     BASE64 = (('A'..'Z').to_a + ('a'..'z').to_a + ('0'..'9').to_a + ['+', '/']).join.freeze
 
     # Note: This is NOT ASCII85 coding alphabet, it's from RFC1924.
