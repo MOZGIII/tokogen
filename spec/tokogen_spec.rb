9074b7e8f8086181a642fdb64faae61c0e87053d
MOZGIII <mike-n@narod.ru>
2017-01-12 15:45:01 +0300

    Added length checking for alphabets at spec/tokogen_spec.rb

--- spec/tokogen_spec.rb
+++ spec/tokogen_spec.rb
@@ -71,7 +71,7 @@ describe Tokogen, '.generator' do
     ]
 
     alphabets.each do |alphabet|
-      expect { Tokogen.generator(alphabet: alphabet).generate(32) }.to_not raise_error
+      expect { Tokogen.generator(alphabet: alphabet).generate(32).size }.to eq 32
     end
   end
 end
